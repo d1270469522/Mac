69b3e63aa3e8df204379691fd797b505096c6f7d
天尽头流浪 <1270469522@qq.com>
2021-05-27 10:44:26 +0800

    2021-03-27 整理删除备份

--- 交接文档/文档/统计任务/sql/最后一次平仓在上周的.sql
+++
@@ -1,3 +0,0 @@
-SELECT  `user_id`,FROM_UNIXTIME(`liqui_time`,'%Y-%m-%d %T') AS liqui_time  FROM `user_order` WHERE `liqui_time`>=1572796800  AND `liqui_time`<1573401600
-GROUP BY `user_id`
-ORDER BY `liqui_time` 
--- 交接文档/文档/统计任务/sql/本周用券下单后无现金交易的未充值用户.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.mobile, u.`id`,sum(`saving_num`) AS money ,COUNT(uo.`id`) AS order_num FROM `user` AS u
-LEFT JOIN `user_saving_order` AS uso ON uso.user_id = u.id AND uso.`status`=1
-LEFT JOIN `user_order` AS uo ON  uo.user_id = u.id AND uo.use_ticket =0
-WHERE u.`reg_time`>=1574611200 
-GROUP BY  u.`id`
-HAVING money IS NULL AND order_num <>0
-LIMIT 10000
